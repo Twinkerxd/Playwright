ffcacc5aa089cad1e1385789b292e07bdd7e7d91
Twinker <@example.com>
2025-03-16 19:06:12 +0100

    Added search input test

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,30 @@
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.Properties;
+
+public class BaseTest {
+
+    protected static Playwright playwright;
+    protected static Browser browser;
+    protected BrowserContext context;
+    protected Page page;
+    protected Properties config;
+
+    @BeforeEach
+    public void setupTest() {
+        playwright = Playwright.create();
+        BrowserType.LaunchOptions launchOptions = new BrowserType.LaunchOptions().setHeadless(false);
+        browser = playwright.chromium().launch(launchOptions);
+        context = browser.newContext(new Browser.NewContextOptions().setViewportSize(1920,1080).setIgnoreHTTPSErrors(true));
+        page = context.newPage();
+        page.navigate("https://github.com/");
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        browser.close();
+        playwright.close();
+    }
+}
---
+++ src/test/java/MainPage.java
@@ -0,0 +1,18 @@
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+
+public class MainPage extends BasePage {
+
+    private final Locator searchInput;
+
+    public MainPage(Page page) {
+        super(page);
+        this.searchInput = page.locator(".search-input");
+    }
+
+    public void search(String text) {
+        searchInput.click();
+        page.fill("#query-builder-test",text);
+        page.press("#query-builder-test", "Enter");
+    }
+}
